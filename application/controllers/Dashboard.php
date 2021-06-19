08ff9457640ee8dd6fad88249c244353227e68a2
parhanhambali <parhanhambali13@gmail.com>
2021-06-19 14:37:31 +0700

    update menu active di helper

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -5,6 +5,8 @@ class Dashboard extends CI_Controller {
     public function __construct()
     {
         parent::__construct();
+
+      //  $this->load->helpers(['menuAktif']);
     } 
 
     public function index()
---
+++ application/helpers/menuAktif_helper.php
@@ -0,0 +1,16 @@
+<?php
+
+// if( $this->router->fetch_class() == 'dashboard') { echo "active"; }
+
+    function menuAktif($class = '') 
+    {
+        $ci =& get_instance();
+
+        if($ci->router->fetch_class() == $class) {
+            return 'active';
+        }
+
+        return false;
+    }
+
+?>
